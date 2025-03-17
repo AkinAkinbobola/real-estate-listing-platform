1304ebd73844a27d78f3ddcb0b932887ced2c95d
Akin Akinbobola <akin.akinbobola@nomba.com>
2025-03-17 15:38:48 +0100

    add user and role entities with security configuration

---
+++ backend/src/main/java/com/akinbobola/backend/security/SecurityConfig.java
@@ -0,0 +1,11 @@
+package com.akinbobola.backend.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+}
---
+++ backend/src/main/java/com/akinbobola/backend/user/User.java
@@ -0,0 +1,88 @@
+package com.akinbobola.backend.user;
+
+import com.akinbobola.backend.role.Role;
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.security.Principal;
+import java.util.Collection;
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "_user")
+@EntityListeners(AuditingEntityListener.class)
+public class User implements UserDetails, Principal {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+
+    private String firstName;
+
+    private String lastName;
+
+    private boolean accountLocked;
+
+    private boolean enabled;
+
+    @ManyToMany
+    private List <Role> roles;
+
+    @Override
+    public Collection <? extends GrantedAuthority> getAuthorities () {
+        return roles.stream().map(role -> new SimpleGrantedAuthority(role.getName())).toList();
+    }
+
+    @Override
+    public String getPassword () {
+        return password;
+    }
+
+    @Override
+    public String getUsername () {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired () {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked () {
+        return !accountLocked;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired () {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled () {
+        return enabled;
+    }
+
+    @Override
+    public String getName () {
+        return email;
+    }
+
+    public String getFullName () {
+        return firstName + " " + lastName;
+    }
+}
