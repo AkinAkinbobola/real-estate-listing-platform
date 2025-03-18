f526e3e645465dfdb89b9b41b58ab42e6b252525
Akin Akinbobola <akin.akinbobola@nomba.com>
2025-03-18 11:12:24 +0100

    remove unused User import from JwtService

--- backend/src/main/java/com/akinbobola/backend/security/JwtService.java
+++ backend/src/main/java/com/akinbobola/backend/security/JwtService.java
@@ -1,6 +1,5 @@
 package com.akinbobola.backend.security;
 
-import com.akinbobola.backend.user.User;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
